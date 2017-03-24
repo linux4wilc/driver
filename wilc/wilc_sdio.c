087a16a44a3467f12856d690065e4387bd8b0687
Pushkar Jambhlekar <pushkar.iit@gmail.com>
2017-03-24 12:58:45 +0530

    drivers/staging/wilc1000: Using __func__ instead of hardcoded function name
    dev_err: replacing hardcoded function name with '%s' and __func__
    
    Signed-off-by: Pushkar Jambhlekar <pushkar.iit@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: AdhamAbozaeid <adham.abozaeid@microchip.com>

--- wilc/wilc_sdio.c
+++ wilc/wilc_sdio.c
@@ -90,7 +90,7 @@ static int wilc_sdio_cmd52(struct wilc *wilc, struct sdio_cmd52 *cmd)
 	sdio_release_host(func);
 
 	if (ret < 0) {
-		dev_err(&func->dev, "wilc_sdio_cmd52..failed, err(%d)\n", ret);
+		dev_err(&func->dev, "%s..failed, err(%d)\n", __func__, ret);
 		return 0;
 	}
 	return 1;
@@ -121,7 +121,7 @@ static int wilc_sdio_cmd53(struct wilc *wilc, struct sdio_cmd53 *cmd)
 	sdio_release_host(func);
 
 	if (ret < 0) {
-		dev_err(&func->dev, "wilc_sdio_cmd53..failed, err(%d)\n", ret);
+		dev_err(&func->dev, "%s..failed, err(%d)\n", __func__, ret);
 		return 0;
 	}
 
