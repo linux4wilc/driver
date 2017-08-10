395d79aead80a2336535a5da58dcef53dd3d4ce0
Aditya Shankar <aditya.shankar@microchip.com>
2017-08-11 03:40:58 +0530

    Update latest WILC drivers
    This is added from the below location
    https://github.com/linux4wilc/linux-at91/tree/master/drivers/staging/wilc1000
    
    Signed-off-by: Aditya Shankar <aditya.shankar@microchip.com>

---
+++ wilc1000/Makefile
@@ -0,0 +1,15 @@
+obj-$(CONFIG_WILC) += wilc.o
+
+ccflags-y += -I$(src)/ -DWILC_ASIC_A0 -DWILC_DEBUGFS
+ccflags-y += -DWILC_BT_COEXISTENCE
+
+wilc-objs := wilc_wfi_cfgoperations.o linux_wlan.o linux_mon.o \
+			coreconfigurator.o host_interface.o \
+			wilc_wlan_cfg.o wilc_debugfs.o \
+			wilc_wlan.o sysfs.o
+
+obj-$(CONFIG_WILC_SDIO) += wilc-sdio.o
+wilc-sdio-objs += wilc_sdio.o wilc_bt.o
+
+obj-$(CONFIG_WILC_SPI) += wilc-spi.o
+wilc-spi-objs += wilc_spi.o wilc_bt.o
---
+++ wilc1000/sysfs.c
@@ -0,0 +1,75 @@
+#include <linux/kobject.h>
+#include "wilc_wfi_cfgoperations.h"
+
+struct p2p_mode
+{
+struct device *dev;
+unsigned int mode;
+};
+
+
+struct p2p_mode *p2p_mode_p;
+
+static struct kobject *p2p_kobj;
+static int p2p_mode;
+static int device_created;
+static struct wilc_vif *vif;
+
+static ssize_t p2p_mode_show(struct kobject *kobj, struct kobj_attribute *attr,
+		      char *buf)
+{
+	return sprintf(buf, "%d\n", p2p_mode);
+}
+
+static ssize_t p2p_mode_store(struct kobject *kobj, struct kobj_attribute *attr,
+	const char *buf, size_t count)
+{
+
+	kstrtoint(buf, 10, &p2p_mode);
+
+	if(p2p_mode)
+		vif->p2p_mode = 1;
+	else
+		vif->p2p_mode = 0;
+
+	return count;
+}
+
+static struct kobj_attribute p2p_mode_attr =
+	__ATTR(mode, 0664, p2p_mode_show, p2p_mode_store);
+
+static struct attribute *p2p_attrs[] = {
+	&p2p_mode_attr.attr,
+	NULL
+};
+
+static struct attribute_group attr_group = {
+   .attrs = p2p_attrs,
+};
+
+void p2p_sysfs_init(struct wilc_vif *vi)
+{
+	int retval;
+	vif = vi;
+
+	if(device_created)
+		return;
+
+	p2p_kobj = kobject_create_and_add("wilc_p2p", NULL);
+	if (!p2p_kobj) {
+		retval = -ENOMEM;
+		return;
+	}
+
+	retval = sysfs_create_group(p2p_kobj, &attr_group);
+	device_created = 1;
+}
+EXPORT_SYMBOL_GPL(p2p_sysfs_init);
+
+void p2p_sysfs_exit(void)
+{
+	device_created=0;
+	sysfs_remove_group(p2p_kobj, &attr_group);
+	kobject_put(p2p_kobj);
+}
+
