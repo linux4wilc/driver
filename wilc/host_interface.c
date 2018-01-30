1f0727a544a09d9dcbe3eaf4e35fecba6139da55
Ajay Singh <ajay.kathat@microchip.com>
2018-01-30 22:20:33 +0530

    staging: wilc1000: rename Handle_Key() and Handle_ConnectTimeout()
    Fix "Avoid camelCase" issue found by checkpatch.pl script.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Reviewed-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: AdhamAbozaeid <adham.abozaeid@microchip.com>

--- wilc/host_interface.c
+++ wilc/host_interface.c
@@ -1292,7 +1292,7 @@ ERRORHANDLER:
 	return result;
 }
 
-static s32 Handle_ConnectTimeout(struct wilc_vif *vif)
+static s32 handle_connect_timeout(struct wilc_vif *vif)
 {
 	s32 result = 0;
 	struct connect_info strConnectInfo;
@@ -1688,8 +1688,8 @@ static s32 Handle_RcvdGnrlAsyncInfo(struct wilc_vif *vif,
 	return result;
 }
 
-static int Handle_Key(struct wilc_vif *vif,
-		      struct key_attr *pstrHostIFkeyAttr)
+static int handle_key(struct wilc_vif *vif,
+					 struct key_attr *pstrHostIFkeyAttr)
 {
 	s32 result = 0;
 	struct wid wid;
@@ -2899,7 +2899,7 @@ static void host_if_work(struct work_struct *work)
 		break;
 
 	case HOST_IF_MSG_KEY:
-		Handle_Key(vif, &msg->body.key_info);
+		handle_key(vif, &msg->body.key_info);
 		break;
 
 	case HOST_IF_MSG_CFG_PARAMS:
@@ -2965,7 +2965,7 @@ static void host_if_work(struct work_struct *work)
 
 	case HOST_IF_MSG_CONNECT_TIMER_FIRED:
 		PRINT_D(vif->ndev, HOSTINF_DBG, "Connect Timeout\n");
-		Handle_ConnectTimeout(vif);
+		handle_connect_timeout(vif);
 		break;
 
 	case HOST_IF_MSG_POWER_MGMT:
