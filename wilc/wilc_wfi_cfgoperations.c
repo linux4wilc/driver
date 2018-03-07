f87e367b2758d3e66c0a5f525987e9edc7954c7d
Ajay Singh <ajay.kathat@microchip.com>
2018-03-07 07:02:23 +0530

    staging: wilc1000: rename CfgScanResult to avoid camelCase
    Fix 'Avoid camelCase' issue found by checkpatch.pl script.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Reviewed-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: AdhamAbozaeid <adham.abozaeid@microchip.com>

--- wilc/wilc_wfi_cfgoperations.c
+++ wilc/wilc_wfi_cfgoperations.c
@@ -439,10 +439,9 @@ static void add_network_to_shadow(struct network_info *nw_info,
 	last_scanned_shadow[ap_index].join_params = pJoinParams;
 }
 
-static void CfgScanResult(enum scan_event scan_event,
-			  struct network_info *network_info,
-			  void *user_void,
-			  void *join_params)
+static void cfg_scan_result(enum scan_event scan_event,
+			    struct network_info *network_info,
+			    void *user_void, void *join_params)
 {
 	struct wilc_priv *priv;
 	struct wiphy *wiphy;
@@ -778,7 +777,7 @@ static int scan(struct wiphy *wiphy, struct cfg80211_scan_request *request)
 					au8ScanChanList,
 					request->n_channels,
 					(const u8 *)request->ie,
-					request->ie_len, CfgScanResult,
+					request->ie_len, cfg_scan_result,
 					(void *)priv, &hidden_ntwk);
 		} else {
 			PRINT_INFO(vif->ndev, CFG80211_DBG,
@@ -787,7 +786,7 @@ static int scan(struct wiphy *wiphy, struct cfg80211_scan_request *request)
 					au8ScanChanList,
 					request->n_channels,
 					(const u8 *)request->ie,
-					request->ie_len, CfgScanResult,
+					request->ie_len, cfg_scan_result,
 					(void *)priv, NULL);
 		}
 	} else {
