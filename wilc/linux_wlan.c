sbourdelin/linux

staging: wilc1000: fix spelling mistake: "Iinitialization" -> "initialization"
Trivial fix to spelling mistake in netdev_err message

Signed-off-by: Colin Ian King <colin.king@canonical.com>

drivers/staging/wilc1000/linux_wlan.c
@@ -813,7 +813,7 @@ _fail_wilc_wlan_:
 		wilc_wlan_cleanup(dev);
 _fail_locks_:
 		wlan_deinit_locks(dev);
-		netdev_err(dev, "WLAN Iinitialization FAILED\n");
+		netdev_err(dev, "WLAN initialization FAILED\n");
 	} else {
 		netdev_dbg(dev, "wilc1000 already initialized\n");
 	}

linux4wilc/driver

Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
Signed-off-by: AdhamAbozaeid <adham.abozaeid@microchip.com>

wilc/linux_wlan.c
@@ -1242,7 +1242,7 @@ _fail_wilc_wlan_:
 		wilc_wlan_cleanup(dev);
 _fail_locks_:
 		wlan_deinit_locks(dev);
-		PRINT_ER(dev, "WLAN Iinitialization FAILED\n");
+		PRINT_ER(dev, "WLAN initialization FAILED\n");
 	} else {
 		PRINT_D(vif->ndev, INIT_DBG, "wilc already initialized\n");
 	}