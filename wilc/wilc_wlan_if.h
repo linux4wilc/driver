c1f1111d1bb8cf82db0f72ef2bc82fcda7892d17
Ajay Singh <ajay.kathat@microchip.com>
2018-04-23 22:03:22 +0530

    staging: wilc1000: rename WID_LOGTerminal_Switch to avoid camelCase
    Fix 'Avoid camelCase' issue found by checkpatch.pl script.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Reviewed-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: AdhamAbozaeid <adham.abozaeid@microchip.com>

--- wilc/wilc_wlan_if.h
+++ wilc/wilc_wlan_if.h
@@ -762,7 +762,7 @@ enum {
 
 	WID_DEL_BEACON			= 0x00CA,
 
-	WID_LOGTerminal_Switch		= 0x00CD,
+	WID_LOG_Terminal_Switch		= 0x00CD,
 	WID_TX_POWER			= 0x00CE,
 	WID_WOWLAN_TRIGGER		= 0X00CF,
 	/*  EMAC Short WID list */
