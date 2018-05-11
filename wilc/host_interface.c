42f0059e7e8dc51e53faa43364b8ed0c2d5dfe7e
Ajay Singh <ajay.kathat@microchip.com>
2018-05-11 13:43:31 +0530

    staging: wilc1000: refactor host_int_parse_assoc_resp_info() to remove unused code
    Remove 'connect_resp_info' structure as most of its elements are not used.
    
    Modified wilc_parse_assoc_resp_info() to directly parse and fill value
    in connect_info structure variable. Remove use of 'assoc_resp_len' variable.
    get_assoc_resp_cap_info() & get_asoc_id() functions are remove as its
    not used anymore.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: AdhamAbozaeid <adham.abozaeid@microchip.com>

--- wilc/host_interface.c
+++ wilc/host_interface.c
@@ -1455,7 +1455,6 @@ static inline void host_int_free_user_conn_req(struct host_if_drv *hif_drv)
 static inline void host_int_parse_assoc_resp_info(struct wilc_vif *vif,
 						  u8 mac_status)
 {
-	struct connect_resp_info *connect_resp_info = NULL;
 	struct connect_info conn_info;
 	struct host_if_drv *hif_drv = vif->hif_drv;
 
@@ -1479,28 +1478,11 @@ static inline void host_int_parse_assoc_resp_info(struct wilc_vif *vif,
 				   "Parsing association response\n");
 			err = wilc_parse_assoc_resp_info(rcv_assoc_resp,
 							 assoc_resp_info_len,
-							 &connect_resp_info);
-			if (err) {
+							 &conn_info);
+			if (err)
 				PRINT_ER(vif->ndev,
 					 "wilc_parse_assoc_resp_info() returned error %d\n",
 					 err);
-			} else {
-				conn_info.status = connect_resp_info->status;
-
-				if (conn_info.status == SUCCESSFUL_STATUSCODE &&
-				    connect_resp_info->ies) {
-					PRINT_D(vif->ndev, HOSTINF_DBG,
-						"Association response received : Successful connection status\n");
-					conn_info.resp_ies = kmemdup(connect_resp_info->ies,
-								     connect_resp_info->ies_len,
-								     GFP_KERNEL);
-					if (conn_info.resp_ies)
-						conn_info.resp_ies_len = connect_resp_info->ies_len;
-				}
-
-				kfree(connect_resp_info->ies);
-				kfree(connect_resp_info);
-			}
 		}
 	}
 
