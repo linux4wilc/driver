f2d1c17336d8063b247e63428db904a33c2f3c45
Adham Abozaeid <adham.abozaeid@microchip.com>
2018-07-23 15:30:50 -0700

    staging: wilc1000: remove 'rxq_entries' from 'wilc' struct
    Removed unnecessary 'rxq_entries' element from 'wilc' struct, as its
    value is not used.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>

--- wilc/linux_wlan.c
+++ wilc/linux_wlan.c
@@ -1735,6 +1735,7 @@ int wilc_netdev_init(struct wilc **wilc, struct device *dev, int io_type,
 		INIT_LIST_HEAD(&wl->txq[i].txq_head.list);
 
 	INIT_LIST_HEAD(&wl->rxq_head.list);
+
 #ifdef DISABLE_PWRSAVE_AND_SCAN_DURING_IP
 	register_inetaddr_notifier(&g_dev_notifier);
 #endif
--- wilc/wilc_wlan.c
+++ wilc/wilc_wlan.c
@@ -630,20 +630,17 @@ static struct txq_entry_t *wilc_wlan_txq_get_next(struct wilc *wilc,
 	return tqe;
 }
 
-static int wilc_wlan_rxq_add(struct wilc *wilc, struct rxq_entry_t *rqe)
+static void wilc_wlan_rxq_add(struct wilc *wilc, struct rxq_entry_t *rqe)
 {
 	struct wilc_vif *vif = wilc->vif[0];
 	
 	if (wilc->quit)
-		return 0;
+		return;
 
 	mutex_lock(&wilc->rxq_cs);
 	list_add_tail(&rqe->list, &wilc->rxq_head.list);
-	wilc->rxq_entries += 1;
-	PRINT_INFO(vif->ndev, RX_DBG,"Number of queue entries: %d\n",
-		   wilc->rxq_entries);
+	PRINT_INFO(vif->ndev, RX_DBG,"Added to RX queue\n");
 	mutex_unlock(&wilc->rxq_cs);
-	return wilc->rxq_entries;
 }
 
 static struct rxq_entry_t *wilc_wlan_rxq_remove(struct wilc *wilc)
@@ -658,7 +655,6 @@ static struct rxq_entry_t *wilc_wlan_rxq_remove(struct wilc *wilc)
 		rqe = list_first_entry(&wilc->rxq_head.list, struct rxq_entry_t,
 				       list);
 		list_del(&rqe->list);
-		wilc->rxq_entries -= 1;
 	} else {
 		PRINT_INFO(vif->ndev, RX_DBG,"Nothing to get from Q\n");
 	}
