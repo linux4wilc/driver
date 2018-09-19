6277a4aba308f0d7fabbd3c09b21b64adeaa725b
Adham Abozaeid <adham.abozaeid@microchip.com>
2018-09-19 15:49:56 -0700

    staging: wilc1000: fix null checks on wilc Currently the pointer wilc is being null checked several times and yet not checked for the final workqueue flush and destroy (which can lead to a null pointer dereference if wilc is null); these missing null checks were overlooked in an earlier core refactoring commit.
    Clean up the code by checking wilc at the start and bailing out
    early if it is null allowing the subsequent null checks to be
    removed, this also fixes the potential null pointer deferences
    on the workqueue flush and destroy calls.
    
    Detected by CoverityScan, CID#1473305 ("Dereference after null check")
    
    Fixes: b3ee105c332e ("staging: wilc1000: refactor code to move initilization in wilc_netdev_init()")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Reviewed-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Adham Abozaeid <adham.abozaeid@microchip.com>

--- wilc/linux_wlan.c
+++ wilc/linux_wlan.c
@@ -1570,12 +1570,15 @@ void wilc_netdev_cleanup(struct wilc *wilc)
 {
 	int i;
 
-	if (wilc && wilc->firmware) {
+	if (!wilc)
+		return;
+
+	if (wilc->firmware) {
 		release_firmware(wilc->firmware);
 		wilc->firmware = NULL;
 	}
 
-	if (wilc && (wilc->vif[0]->ndev || wilc->vif[1]->ndev)) {
+	if ((wilc->vif[0]->ndev || wilc->vif[1]->ndev)) {
 		for (i = 0; i < NUM_CONCURRENT_IFC; i++)
 			if (wilc->vif[i]->ndev)
 				if (wilc->vif[i]->mac_opened)
