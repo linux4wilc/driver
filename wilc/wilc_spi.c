15617265f475bdf30b27ab14580701034ee795dd
Adham Abozaeid <adham.abozaeid@microchip.com>
2019-10-18 18:13:01 -0700

    staging: wilc1000: Preparing for release 15.3RC2
    Signed-off-by: Adham Abozaeid <adham.abozaeid@microchip.com>

--- wilc/wilc_spi.c
+++ wilc/wilc_spi.c
@@ -238,7 +238,7 @@ static struct spi_driver wilc_spi_driver = {
 };
 module_spi_driver(wilc_spi_driver);
 MODULE_LICENSE("GPL");
-MODULE_VERSION("15.3_RC2_DEV");
+MODULE_VERSION("15.3_RC2");
 
 static int spi_data_rsp(struct wilc *wilc, u8 cmd)
 {
